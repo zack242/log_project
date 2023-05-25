ae0dc1cf7689d4305b06f11171edf96c768e06c1
zack242 <zakaria_009@hotmail.fr>
2023-05-25 17:33:03 +0200

    Add utils function

---
+++ utils.py
@@ -0,0 +1,43 @@
+import os
+import json
+
+
+# Get the list of log files in the logs folder
+def get_log_name():
+    arr = os.listdir("logs")
+    return arr
+
+
+# Get the date of a given log file
+def get_log_date(log_name):
+    return log_name.split("-")[1].split(".")[0]
+
+
+def get_last_7_days(path):
+    logs_to_process = os.listdir(path)
+    logs_to_process.sort()
+    return logs_to_process
+
+
+# Check if a string is a country code
+def is_country_code(string):
+    if len(string) == 2:
+        for char in string:
+            if not char.isalpha():
+                return False
+        return True
+    return False
+
+
+# We save intermediate results to disk
+def save_dict_to_file(dictionary, dir, date):
+    os.makedirs(dir, exist_ok=True)
+    with open(dir + "/" + date, "w") as file:
+        json.dump(dictionary, file)
+
+
+# We load intermediate results from disk
+def load_dict_from_file(file_path):
+    with open(file_path, "r") as file:
+        dictionary = json.load(file)
+    return dictionary
